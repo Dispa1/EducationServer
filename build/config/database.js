176b1921d3b1dea2f653f353a131e6083d863e1d
«Denis» <denis.tretyakov003@gmail.com>
2024-03-03 23:11:20 +0500

    dp commit

--- build/config/database.js
+++ build/config/database.js
@@ -4,7 +4,7 @@ dotenv.config();
 const sequelize = new Sequelize(process.env.POSTGRES_DB, process.env.POSTGRES_USER, process.env.POSTGRES_PASSWORD, {
     host: process.env.POSTGRES_HOST,
     dialect: 'postgres',
-    port: parseInt(process.env.POSTGRES_PORT || '5433'),
+    port: parseInt(process.env.POSTGRES_PORT || '5453'),
 });
 export default sequelize;
 //# sourceMappingURL=database.js.map
--- src/config/migrations/20240302125358-username-to-message.js
+++ src/config/migrations/20240302125358-username-to-message.js
@@ -3,13 +3,13 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    await queryInterface.addColumn('Message', 'userName', {
+    await queryInterface.addColumn('Messages', 'userName', {
       type: Sequelize.DataTypes.STRING,
       allowNull: true
     })
   },
 
   async down (queryInterface, Sequelize) {
-    await queryInterface.removeColumn('Message', 'userName');
+    await queryInterface.removeColumn('Messages', 'userName');
   }
 };
